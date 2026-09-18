acc8d9a6ae0dddaf885c7c8e87ec2c8d06f6e083
ExistentialThinker <hpaprosk@ualberta.ca>
2026-09-18 16:57:36 -0600

    Commit Changes

---
+++ Assets/ImageRot.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageRot : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(0, 0, speed*Time.deltaTime);
+    }
+}
